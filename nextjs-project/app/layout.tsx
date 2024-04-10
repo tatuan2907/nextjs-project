aa62a0d3791dcf48c1eed91195fe4dd8677d7b76
minh le <117999133+sixxson@users.noreply.github.com>
2024-04-11 02:01:17 +0700

    update layout home

--- nextjs-project/app/layout.tsx
+++ nextjs-project/app/layout.tsx
@@ -24,6 +24,12 @@ export default function RootLayout({
             <Header />
             {children}
           </div>
+          <footer className="bg-gray-200">
+            <div className="container mx-auto px-6 py-3 flex justify-between items-center">
+              <a href="#" className="text-xl font-bold text-gray-500 hover:text-gray-400">Brand</a>
+              <p className="py-2 text-gray-500 sm:py-0">All rights reserved</p>
+            </div>
+          </footer>
         </Providers>
 
       </body>
