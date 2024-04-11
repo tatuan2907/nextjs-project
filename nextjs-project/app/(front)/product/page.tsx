934ebaebc297b3181ca3f73f7f882c0081a0dc5e
minh le <117999133+sixxson@users.noreply.github.com>
2024-04-11 11:18:32 +0700

    debug page products

--- nextjs-project/app/(front)/product/page.tsx
+++ nextjs-project/app/(front)/product/page.tsx
@@ -1,16 +1,21 @@
-import Productitem from "@/components/products/ProductItem"
-import data from "@/lib/data"
 
-export default function Product() {
-    return(
-        <div className="">
-        <h2 className="text-2xl py-2">Latest Products</h2>
-        <div className="grid grid-col-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-            {
-                data.products.map((product) => (
-                    <Productitem key={product.slug}  product={product} />
-                ))
-            }
-        </div>
-    </div>
-    )}
+import Productitem from "@/components/products/ProductItem";
+import productServices from "@/lib/services/productService";
+import { converDocToObj } from "@/lib/utils";
+
+export default async function Prodcut() {
+    const latesProducts = await productServices.getLatest()
+    return (
+        <>
+            <div className="flex flex-col w-full">
+                <div className="divider"><h2 className="text-2xl py-2">Products</h2></div>
+            </div>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+                {latesProducts.map((product) => (
+                    <Productitem key={product.slug} product={converDocToObj(product)} />
+                ))}
+            </div>
+        </>
+    )
+}
+
